8195fbac5c70d0bb72d2526754a98cec3850c519
timmyjinks <tysonjjenkins.github@gmail.com>
2025-11-23 21:02:08 -0700

    Add disko package

--- nixos/TYSONCLOUD/flake.nix
+++ nixos/TYSONCLOUD/flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    disko.url = "github:nix-community/disko";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, disko, ... }:
     {
       nixosConfigurations.tysocloud-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          disko.nixosModules.disko
           ./tysoncloud.nix
         ];
       };

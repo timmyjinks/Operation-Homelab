352c16f0480cc00b48de4908ca995a8b0e56305a
timmyjinks <tysonjjenkins.github@gmail.com>
2026-03-22 17:14:53 -0600

    Rename config file

--- nixos/TYSONCLOUD/configuration.nix
+++ nixos/TYSONCLOUD/configuration.nix
@@ -23,11 +23,11 @@
   users.users.timmy = {
     isNormalUser = true;
     hashedPassword = "$y$j9T$gu4zW.T0lNMbGoEJPmnxZ/$QUr9z7NfC34R49E..bNrx8QbJW1DSPOO.qe2WcFBkr0";
-    extraGroups = [ "wheel" "docker"];
-    openssh.authorizedKeys.keys = 
-    [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjySdMyjMatA9pdrGPgOn9G7D6ijFOZxWF+GYTcJgofTbPbNwPx3CJQ+lhyT6bep8hUAvUowpwSCXXy8jvBRaadPEfaJ30omdOIFjPPlif4dcfURb5Zp2Cvws0UxGPy3fyDgtdur0UGfcyB5+oJ2oaZvxJc4af4EPXtumIIN2r3AUO1tYacRucaFvAH0rPhJ7w7+vD+OMD/OVj36PYNRPvAUdnQRfDedr7uLJTaoZGKNy2RCq+k6td2jh9CC2qCefry/04t31ymvoG9CDP6CEUt6neHkZjFlUWbWH1Ud7FeZZwcK3HsACtQbO6+BoxbHDPYgg2EkGWevYsFQS7HslyWzbv8SPXVMBKfEt7qkUszNY6XQm0yxrlOeZhoubaq2+2QhGF+li6lcxcTU6fITepKfPiXgonje7iQPaLx20CPEJJ7rSQiRcVIWQgdi8fNec/aQuGG6ML+BrKP68h2Fe1/L/MtE6eyjqgvmYYIq0Nxb85Cj8Lio4tshvN4/JGYkk= tysonjjenkins.github@gmail.com"
-    ];
+    extraGroups = [ "wheel" "docker" ];
+    openssh.authorizedKeys.keys =
+      [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQQo/lthp8a7idO9NW3VOEM08ORFc46RPDOqNmIFgCe timmy@nobara-pc"
+      ];
   };
 
   users.users.traefik.extraGroups = [ "docker" ];
